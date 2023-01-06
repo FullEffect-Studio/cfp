86ffa9443983433709af67c3a7647d1d1428cbc0
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-06 10:54:40 +0000

    Layouting and styling

--- src/components/home/ClientKnowHow.jsx
+++ src/components/home/ClientKnowHow.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const ClientKnowHowStyle = styled.main`
+  @import url("https://fonts.googleapis.com/css2?family=Rubik+Bubbles&display=swap");
   width: 70vw;
-  height: 500px;
+  height: 600px;
   background-color: rgb(241, 179, 62);
   position: absolute;
   top: 90%;
@@ -20,43 +21,44 @@ const ClientKnowHowStyle = styled.main`
       color: white;
       font-size: 18px;
       font-weight: 600;
-      border-bottom: 0.31rem solid aliceblue;
+      // border-bottom: 0.31rem solid aliceblue;
 
       & li:nth-of-type(5) {
-        padding-bottom: 4px;
+        padding-bottom: 1rem;
       }
     }
   }
 
   .report-img {
     object-fit: contain;
-    width: 50px !important;
-    height: 50px;
+    width: 80px !important;
+    height: 80px;
   }
 
-  .column-2{
+  .column-2 {
     width: 50%;
     display: flex;
     flex-direction: column;
   }
 
-  .report-links{
+  .report-links {
     display: flex;
     justify-content: space-around;
     color: black;
     text-decoration: none;
     padding: 3px;
 
-    &:hover{
+    &:hover {
       background-color: white;
       color: #333;
     }
   }
 
-  .report-list{
+  .report-list {
     display: flex;
     flex-direction: column;
-    gap: .7rem;
+    gap: 0.7rem;
+    align-items: center;
   }
 `;
 
@@ -64,37 +66,88 @@ const ClientKnowHow = () => {
   return (
     <ClientKnowHowStyle>
       <div className="column-1">
-        <ol className="steps-to-report">
+        <ol className="steps-to-report" style={{ listStyleType: "none" }}>
           <h3 className="text-secondary">Steps to report a problem</h3>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing!</li>
-          <li>Lorem, ipsum dolor sit amet adipisicing.</li>
-          <li>Lorem ipsum dolor sit amet coelit.</li>
-          <li>Lorem ipsum dolor, sit amet consectetu</li>
-          <li>Lorem ipsum dolor sit amet consectetur.</li>
+          <li>
+            <img src="images/1.png" alt="One" width="44px" /> Lorem ipsum dolor
+            sit amet consectetur!
+          </li>
+          <li>
+            <img src="images/2.png" alt="One" width="44px" /> Lorem, ipsum dolor
+            sit amet adipisicing.
+          </li>
+          <li>
+            <img src="images/3.png" alt="One" width="44px" /> Lorem ipsum dolor
+            sit amet coelit.
+          </li>
+          <li>
+            <img src="images/4.png" alt="One" width="44px" /> Lorem ipsum dolor,
+            sit amet consectetu
+          </li>
+          {/* <li><img src="images/5.png" alt="One" width="44px" /> Lorem ipsum dolor sit amet consectetur.</li> */}
         </ol>
-        <div className="row some-statistics" style={{paddingLeft: "1rem", width: "100% !important"}}>
+        <hr style={{width: "80%", height:"1rem !important", margin: "2rem auto"}} />
+        <div
+          className="row some-statistics text-center"
+          style={{ paddingLeft: "1rem", width: "100% !important" }}
+        >
           <div className="col-lg-4">
             {" "}
-            <div className="stat-value">12,000</div>
-            <div className="stat-description">Description 1</div>
+            <div
+              className="stat-value"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+                justifyContent: "center",
+              }}
+            >
+              <span className="text-primary" style={{ fontSize: "28px", fontWeight: "900",fontFamily: "Rubik Bubbles, cursive" }}>
+                {" "}
+                12,000
+              </span>{" "}
+              <span className="stat-description text-light" style={{fontSize:"19px", fontWeight: "700"}}>reports recently</span>
+            </div>
           </div>
           <div className="col-lg-4">
             {" "}
-            <div className="stat-value">5,000</div>
-            <div className="stat-description">Description 2</div>
+            <div
+              className="stat-value"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+                justifyContent: "center",
+              }}
+            >
+              <span className="text-primary" style={{ fontSize: "28px", fontWeight: "900",fontFamily: "Rubik Bubbles, cursive" }}>9,000</span>
+              <span className="stat-description text-light" style={{fontSize:"19px", fontWeight: "700"}}>report for past month</span>
+            </div>{" "}
           </div>
           <div className="col-lg-4">
             {" "}
-            <div className="stat-value">483,000</div>
-            <div className="stat-description">Description 3</div>
+            <div
+              className="stat-value"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+                justifyContent: "center",
+              }}
+            >
+              <span className="text-primary" style={{ fontSize: "28px", fontWeight: "900",fontFamily: "Rubik Bubbles, cursive" }}>
+                483,000
+              </span>
+              <span className="stat-description text-light" style={{fontSize:"19px", fontWeight: "700"}}>report for last year</span>
+            </div>{" "}
           </div>
         </div>
       </div>
       <div className="column-2">
         <h3>Recently posted problems</h3>
-        <ol className="report-list">
+        <ol className="report-list" style={{ listStyleType: "disc" }}>
           <li>
-            <a href="/"  className="report-links">
+            <a href="/" className="report-links">
               Lorem ipsum dolor sit amet consectetur. Lorem, ipsum dolor. Lorem
               ipsum dolor sit.
               <img
@@ -148,7 +201,6 @@ const ClientKnowHow = () => {
             </a>
           </li>
         </ol>
-        
       </div>
     </ClientKnowHowStyle>
   );
