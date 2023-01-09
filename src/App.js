63800ecbfe40c3169d418dada6cbd5a7917a4d46
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-09 13:47:14 +0000

    Public officer login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminLoginForm from "./components/Auth/AdminLoginForm";
 import CommnityWorkerLoginForm from "./components/Auth/CommnityWorkerLoginForm";
+import PublicOfficerLoginForm from "./components/Auth/PublicOfficerLoginForm";
 import LandingPage from "./components/LandingPage";
 import ReportAProblem from "./components/report/ReportAProblem";
 import "./index.css"
@@ -14,6 +15,7 @@ export const App = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="auth/admin/signin/" element={<AdminLoginForm />} />
           <Route path="auth/community-worker/signin/" element={<CommnityWorkerLoginForm />} />
+          <Route path="auth/public-officer/signin/" element={<PublicOfficerLoginForm />} />
           <Route path="report-a-problem/" element={<ReportAProblem />} />
         </Routes>
       </Router>
