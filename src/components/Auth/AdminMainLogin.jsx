40f930b3922077c454690aefcadf1aa9012a88db
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-09 12:24:50 +0000

    Added an image to the login page

--- src/components/Auth/AdminMainLogin.jsx
+++ src/components/Auth/AdminMainLogin.jsx
@@ -5,11 +5,25 @@ const StyledForm = styled.main`
     height: 500px;
     background: rgb(241, 179, 62);
     margin: 3rem auto;
+
+    .col-md-7{
+      height: 500px;
+      
+      & img{
+        height: 100%;
+      }
+    }
+
 `;
 const AdminMainLogin = () => {
   return (
     <StyledForm>
-        
+        <div className="row">
+          <div className="col-md-7">
+            <img src="/images/womanhone.jpeg" alt="my-image" />
+          </div>
+          <div className="col-md-5"></div>
+        </div>
     </StyledForm>
   )
 }
