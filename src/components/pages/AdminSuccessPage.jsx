ed45818e66053c83dab989dfd47fd4702d56f550
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-10 00:35:20 +0000

    A lot of stuffs

---
+++ src/components/pages/AdminSuccessPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AdminDashboardSidebar } from './AdminDashboardSidebar'
+import AdminNavBar from './AdminNavBar'
+
+const AdminSuccessPage = () => {
+  return (
+    <div>
+        <AdminDashboardSidebar />
+    </div>
+  )
+}
+
+export default AdminSuccessPage
