e4476bceba3fb3e69a7f4045b653cc9f32a66b50
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-11 17:44:23 +0000

    Adding a draggerble marker

--- src/components/report/MyReportAProblem.jsx
+++ src/components/report/MyReportAProblem.jsx
@@ -1,10 +1,51 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
 import styled from "styled-components";
 import NaviBar from "../home/Navibar";
 import PageFooter from "../PageFooter";
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import {Icon} from 'leaflet'
+
+const center = {
+  lat: 51.505,
+  lng: -0.09,
+}
+
+function DraggableMarker() {
+  const [draggable, setDraggable] = useState(false)
+  const [position, setPosition] = useState(center)
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        if (marker != null) {
+          setPosition(marker.getLatLng())
+        }
+      },
+    }),
+    [],
+  )
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d)
+  }, [])
+
+  return (
+    <Marker
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}>
+      <Popup minWidth={90}>
+        <span onClick={toggleDraggable}>
+          {draggable
+            ? 'Marker is draggable'
+            : 'Click here to make marker draggable'}
+        </span>
+      </Popup>
+    </Marker>
+  )
+}
 
 function LocationMarker() {
   const [position, setPosition] = useState(null)
