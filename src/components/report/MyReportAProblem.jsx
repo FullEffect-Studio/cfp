ee4853592ec64736610e3b90b7ff1b2f4f4e36d7
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-11 17:23:02 +0000

    Leaflet map confs

--- src/components/report/MyReportAProblem.jsx
+++ src/components/report/MyReportAProblem.jsx
@@ -1,6 +1,27 @@
-import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
 import styled from "styled-components";
+import NaviBar from "../home/Navibar";
+import PageFooter from "../PageFooter";
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
 
 const StyledReportPage = styled.main`
   width: 100vw;
@@ -14,30 +35,32 @@ const StyledReportPage = styled.main`
 `;
 
 const ReportAProblem = () => {
+  
   return (
-    <StyledReportPage>
-      <div className="row">
-        <div className="col-md-5">kjc skm</div>
-        <div className="col-md-7">
-          <MapContainer
-            center={[51.505, -0.09]}
-            zoom={13}
-            scrollWheelZoom={false}
-            style={{ width: "100%", height: "100vh" }}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
+    <>
+      <NaviBar />
+      <StyledReportPage>
+        <div className="row">
+          <div className="col-md-5">kjc skm</div>
+          <div className="col-md-7">
+            <MapContainer
+              center={[51.505, -0.09]}
+              zoom={13}
+              scrollWheelZoom={true}
+              style={{ width: "100%", height: "100vh" }}
+              
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <LocationMarker />
+            </MapContainer>
+          </div>
         </div>
-      </div>
-    </StyledReportPage>
+      </StyledReportPage>
+      <PageFooter />
+    </>
   );
 };
 
