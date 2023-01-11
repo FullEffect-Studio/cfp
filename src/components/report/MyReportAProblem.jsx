0bde4c8f687bc971f7a177bf47a6d8b7cd91acdc
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-11 18:04:36 +0000

    Added draggable functionality

--- src/components/report/MyReportAProblem.jsx
+++ src/components/report/MyReportAProblem.jsx
@@ -1,69 +1,104 @@
 import React, { useCallback, useMemo, useRef, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
 import styled from "styled-components";
 import NaviBar from "../home/Navibar";
 import PageFooter from "../PageFooter";
-import markerIconPng from "leaflet/dist/images/marker-icon.png"
-import {Icon} from 'leaflet'
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
 
 const center = {
   lat: 51.505,
   lng: -0.09,
-}
+};
 
 function DraggableMarker() {
-  const [draggable, setDraggable] = useState(false)
-  const [position, setPosition] = useState(center)
-  const markerRef = useRef(null)
+  const [draggable, setDraggable] = useState(false);
+  const [position, setPosition] = useState(center);
+  const markerRef = useRef(null);
   const eventHandlers = useMemo(
     () => ({
       dragend() {
-        const marker = markerRef.current
+        const marker = markerRef.current;
         if (marker != null) {
-          setPosition(marker.getLatLng())
+          setPosition(marker.getLatLng());
         }
       },
     }),
-    [],
-  )
+    []
+  );
   const toggleDraggable = useCallback(() => {
-    setDraggable((d) => !d)
-  }, [])
+    setDraggable((d) => !d);
+  }, []);
 
   return (
     <Marker
       draggable={draggable}
       eventHandlers={eventHandlers}
       position={position}
-      ref={markerRef}>
+      ref={markerRef}
+    >
       <Popup minWidth={90}>
         <span onClick={toggleDraggable}>
           {draggable
-            ? 'Marker is draggable'
-            : 'Click here to make marker draggable'}
+            ? "Marker is draggable"
+            : "Click here to make marker draggable"}
         </span>
       </Popup>
     </Marker>
-  )
+  );
 }
 
 function LocationMarker() {
-  const [position, setPosition] = useState(null)
+  const [position, setPosition] = useState(null);
+  const [draggable, setDraggable] = useState(false);
+  const markerRef = useRef(null);
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          setPosition(marker.getLatLng());
+        }
+      },
+    }),
+    []
+  );
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d);
+  }, []);
   const map = useMapEvents({
     click() {
-      map.locate()
+      map.locate();
     },
     locationfound(e) {
-      setPosition(e.latlng)
-      map.flyTo(e.latlng, map.getZoom())
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
     },
-  })
+  });
 
   return position === null ? null : (
-    <Marker position={position} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+    <Marker
+      position={position}
+      icon={
+        new Icon({
+          iconUrl: markerIconPng,
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+        })
+      }
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      ref={markerRef}
+    >
       <Popup>You are here</Popup>
     </Marker>
-  )
+  );
 }
 
 const StyledReportPage = styled.main`
@@ -78,7 +113,6 @@ const StyledReportPage = styled.main`
 `;
 
 const ReportAProblem = () => {
-  
   return (
     <>
       <NaviBar />
@@ -91,7 +125,6 @@ const ReportAProblem = () => {
               zoom={13}
               scrollWheelZoom={true}
               style={{ width: "100%", height: "100vh" }}
-              
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
