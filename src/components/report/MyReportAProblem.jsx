068163ced92ff525b688fa3f53ee94623c7e1c7e
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-11 18:05:22 +0000

    Clean code

--- src/components/report/MyReportAProblem.jsx
+++ src/components/report/MyReportAProblem.jsx
@@ -12,47 +12,6 @@ import PageFooter from "../PageFooter";
 import markerIconPng from "leaflet/dist/images/marker-icon.png";
 import { Icon } from "leaflet";
 
-const center = {
-  lat: 51.505,
-  lng: -0.09,
-};
-
-function DraggableMarker() {
-  const [draggable, setDraggable] = useState(false);
-  const [position, setPosition] = useState(center);
-  const markerRef = useRef(null);
-  const eventHandlers = useMemo(
-    () => ({
-      dragend() {
-        const marker = markerRef.current;
-        if (marker != null) {
-          setPosition(marker.getLatLng());
-        }
-      },
-    }),
-    []
-  );
-  const toggleDraggable = useCallback(() => {
-    setDraggable((d) => !d);
-  }, []);
-
-  return (
-    <Marker
-      draggable={draggable}
-      eventHandlers={eventHandlers}
-      position={position}
-      ref={markerRef}
-    >
-      <Popup minWidth={90}>
-        <span onClick={toggleDraggable}>
-          {draggable
-            ? "Marker is draggable"
-            : "Click here to make marker draggable"}
-        </span>
-      </Popup>
-    </Marker>
-  );
-}
 
 function LocationMarker() {
   const [position, setPosition] = useState(null);
