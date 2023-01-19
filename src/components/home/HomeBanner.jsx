953858277e4f564c8ec7d67144f8b78d4bf6a9ed
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-19 07:54:49 +0000

    Homebanner height adjustment

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -54,7 +54,7 @@ const HomeBannerStyle = styled.main`
       justify-content: center;
       gap: 1rem;
       width: 100%;
-      height: calc(100vh + 6rem);;
+      height: calc(100vh + 10rem);;
 
       & .greeting{
         width: 100%;
