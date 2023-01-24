16420844780f3f82637707c98883a58c22719a79
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-24 19:12:56 +0000

    Styling Home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,17 +3,22 @@ import HomeBanner from './HomeBanner'
 import ItemsGroup from './ItemsGroup'
 import NaviBar from './Navibar'
 import ReportAProblem from './ReportAProblem'
+import styled from 'styled-components'
 
+const StyledHome = styled.main`
+  width: 100vw;
+  height: max-content;
+`;
 
 const Home = () => {
   return (
-    <div>
+    <StyledHome>
         <NaviBar />
         <HomeBanner /> 
         <ReportAProblem /> 
         <ItemsGroup />
 
-    </div>
+    </StyledHome>
   )
 }
 
