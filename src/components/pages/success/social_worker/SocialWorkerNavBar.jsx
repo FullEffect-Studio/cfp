4f8833e8a1a3e224b04bfbc7d14e4954a71d10e3
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-30 22:56:23 +0000

    Social worker nav

--- src/components/pages/success/social_worker/SocialWorkerNavBar.jsx
+++ src/components/pages/success/social_worker/SocialWorkerNavBar.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const SocialWorkerNavBar = () => {
   return (
-    <div className='w-100 bg-teal-500 flex justify-around h-12 text-purple-100'>
-      <div className="flex items-center">
-        CFP
-      </div>
-      <ul className='flex list-none justify-between w-2/5 h-full items-center'>
-        <li className=' hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7'>Upload Data</li>
-        <li className=' hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7'>Review Statistics</li>
-        <li className=' hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7'>Notifications</li>
+    <div className="w-100 bg-teal-500 flex justify-around h-12 text-purple-100">
+      <div className="flex items-center">CFP</div>
+      <ul className="flex list-none justify-between w-2/5 h-full items-center">
+        <li className=" hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7">
+          Upload Data
+        </li>
+        <li className=" hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7">
+          Review Statistics
+        </li>
+        <li className=" hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7">
+          Notifications
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SocialWorkerNavBar
+export default SocialWorkerNavBar;
