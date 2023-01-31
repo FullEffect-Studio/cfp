948c2306ef502214c3369c4a6b85c35a105450d7
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-31 08:26:33 +0000

    Working on Social worker page

--- src/components/pages/success/social_worker/SocialWorkerNavBar.jsx
+++ src/components/pages/success/social_worker/SocialWorkerNavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SocialWorkerNavBar = () => {
   return (
-    <div className="w-100 bg-teal-500 flex justify-around h-12 text-purple-100">
+    <div className="w-100 bg-zinc-900 flex justify-around h-12 text-purple-100 fixed top-0">
       <div className="flex items-center">CFP</div>
       <ul className="flex list-none justify-between w-2/5 h-full items-center">
         <li className=" hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7">
