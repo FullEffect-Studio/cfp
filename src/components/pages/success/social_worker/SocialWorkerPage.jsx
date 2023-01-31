0b64b3daf73a08fcb12e4c33d432734166e184b4
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-31 08:25:38 +0000

    Working on Social worker page

---
+++ src/components/pages/success/social_worker/SocialWorkerPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SocialWorkerHeader from './SocialWorkerHeader'
+import SocialWorkerNavBar from './SocialWorkerNavBar'
+
+const SocialWorkerPage = () => {
+  return (
+    <div className=' h-max'>
+        <SocialWorkerNavBar />
+        <SocialWorkerHeader />
+    </div>
+  )
+}
+
+export default SocialWorkerPage
