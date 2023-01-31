055b64ee4294fe539730a3243f6d867cf826160e
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-31 08:27:06 +0000

    Working on Social worker page

--- src/components/pages/success/social_worker/SocialWorkerHeader.jsx
+++ src/components/pages/success/social_worker/SocialWorkerHeader.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
 
-const StyledHeader = styled.header`
-  background: url("public/images/hero_bg.jpg");
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-`
 const SocialWorkerHeader = () => {
   return (
-    <div style={{"bacground": "url()"}}>
-        <div className="hero"></div>        
-    </div>
+    <main style={{height: "2000px"}}>
+      <div className='sw-section'></div>
+        <div className="hero">Kofi</div>        
+    </main>
   )
 }
 
