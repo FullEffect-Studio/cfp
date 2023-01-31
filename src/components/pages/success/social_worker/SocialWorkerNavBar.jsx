623bc72b4d6890f2997cb3b8f988af70b59490e4
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-31 08:47:01 +0000

    Still on Social worker navigation

--- src/components/pages/success/social_worker/SocialWorkerNavBar.jsx
+++ src/components/pages/success/social_worker/SocialWorkerNavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SocialWorkerNavBar = () => {
   return (
-    <div className="nav_sw w-100 bg-zinc-900 flex justify-around h-12 text-purple-100 fixed top-0">
+    <nav className="nav_sw w-100 bg-zinc-900 flex justify-around h-12 text-purple-100 fixed top-0">
       <div className="flex items-center">CFP</div>
       <ul className="flex list-none justify-between w-2/5 h-full items-center">
         <li className=" hover:bg-zinc-900 hover:text-yellow-100 h-full flex items-center px-7">
@@ -15,7 +15,7 @@ const SocialWorkerNavBar = () => {
           Notifications
         </li>
       </ul>
-    </div>
+    </nav>
   );
 };
 
