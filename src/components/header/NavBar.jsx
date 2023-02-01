5b2712dd575f66ec49edcde4de33c3b2c3934be5
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-02-01 03:48:22 +0000

    Getting sassy :)

---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const NavBar = () => {
+  return (
+    <Nav>
+        <div className="logo">
+            <img src="" alt="img" />
+            <p>Navbar</p>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+    </Nav>
+  )
+}
+
+export default NavBar
